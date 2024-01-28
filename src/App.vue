d94a81ea7ae0a7d859159b7feca73b73b396f88a
Jarrod Whitley <jarrodwhitley@gmail.com>
2024-01-27 18:40:57 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="app">
+        <Questionnaire :client-data="clientData"/>
+    </div>
+</template>
+
+<script>
+import Questionnaire from './components/Questionnaire.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Questionnaire
+    },
+    data() {
+        return {
+            clientData: {
+                name: 'Donut',
+                logo: 'https://donut.media/cdn/shop/files/DONM-0114_StoreRetheme_SiteExp_Logo-01_x300.png?v=1670446627',
+                intro: {
+                    title: 'Welcome to Donut!',
+                    text: 'Thank you for taking the time to fill out this questionnaire. We look forward to getting to know what makes you tick!',
+                    ctaText: 'Start'
+                },
+                fields: [
+                    {
+                        label: 'First Name',
+                        id: 'fname',
+                    },
+                    {
+                        label: 'Last Name',
+                        id: 'lname',
+                    },
+                    {
+                        label: 'Email',
+                        id: 'email',
+                    },
+                    {
+                        label: 'What is your favorite color?',
+                        id: 'color',
+                    }
+                ],
+                primaryColor: '#fcd510',
+                secondaryColor: '#E5202A'
+            }
+        }
+    }
+}
+</script>
---
+++ src/components/Questionnaire.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="questionnaire">
+        <div class="questionnaire__header" :style="'background-color:' + data.primaryColor">
+            <img class="questionnaire__header__logo h-full w-auto" :src="data.logo" alt="client logo"/>
+        </div>
+        <div class="questionnaire__intro h-screen flex flex-col justify-content-center align-items-center">
+            <h1 class="questionnaire__intro__title" v-text="data.intro.title"></h1>
+            <p class="questionnaire__intro__text" v-text="data.intro.text"></p>
+        </div>
+        <form class="questionnaire__form">
+            <div class="questionnaire__form__field h-screen w-screen"
+                 v-for="field in data.fields">
+                <label :for="field.id">{{ field.label }}
+                    <input :id="field.id"/>
+                </label>
+            </div>
+        </form>
+        <div class="questionnaire__footer" :style="'background-color:' + data.secondaryColor">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Questionnaire',
+    props: {
+        clientData: Array
+    },
+    data() {
+        return {
+            data: this.clientData,
+
+        }
+    }
+}
+</script>
